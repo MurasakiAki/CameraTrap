4c0bffae57b393273f9dd6b9d4771b724dda4243
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-02-26 20:52:58 +0100

    Better script

--- motcam_script.py
+++ motcam_script.py
@@ -13,74 +13,87 @@ config.read(config_path)
 
 mode = config.get('config', 'mode')
 time_limit = int(config.get('config', 'time'))
-
-# create capture object
-cap = cv2.VideoCapture(0)
-# create background subtractor
-fgbg = cv2.createBackgroundSubtractorMOG2()
+capture_interval = int(config.get('config', 'capture_interval'))
 
 if mode == 'photo':
-    print("Capturing photo...")
-    while True:
-        # read a frame
-        ret, frame = cap.read()
-        # apply background subtraction
-        fgmask = fgbg.apply(frame)
-        # count non-zero pixels in the mask
-        count = cv2.countNonZero(fgmask)
-        # if motion is detected, take a photo
-        if count > 100:
-            image_path = f'motion_on{time.ctime()}.png'
+    print("Capturing photos...")
+else:
+    print("Recording videos...")
+
+# initialize video capture
+cap = cv2.VideoCapture(0)
+
+# initialize motion detection parameters
+motion_frame = None
+motion_detected = False
+
+# start capture loop
+while True:
+    # capture a frame
+    ret, frame = cap.read()
+    
+    # convert frame to grayscale for motion detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    
+    # set initial motion frame
+    if motion_frame is None:
+        motion_frame = gray
+        continue
+    
+    # calculate difference between current frame and motion frame
+    frame_delta = cv2.absdiff(motion_frame, gray)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    
+    # dilate the thresholded image to fill in holes
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    
+    # find contours in the thresholded image
+    contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # check if motion is detected
+    for c in contours:
+        if cv2.contourArea(c) < 500:
+            continue
+        motion_detected = True
+    
+    # reset motion frame if no motion is detected
+    if not motion_detected:
+        motion_frame = gray
+    
+    # capture photo or record video if motion is detected
+    if motion_detected:
+        if mode == 'photo':
+            # capture a single frame
+            image_path = f'motion_on_{time.time()}.png'
             cv2.imwrite('Captures/' + image_path, frame)
             print("Photo captured!")
-            break
-        # display the frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-elif mode == 'video':
-    print("Recording video...")
-    # initialize video writer
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('motion_video.avi', fourcc, 20.0, (640, 480))
-    # start recording
-    start_time = time.time()
-    while True:
-        # read a frame
-        ret, frame = cap.read()
-        # apply background subtraction
-        fgmask = fgbg.apply(frame)
-        # count non-zero pixels in the mask
-        count = cv2.countNonZero(fgmask)
-        # if motion is detected, start recording
-        if count > 100:
-            print("Motion detected! Recording video...")
+        else:
+            # initialize video writer
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            out = cv2.VideoWriter(f'motion_video_{time.time()}.avi', fourcc, 20.0, (640, 480))
+            # start recording
+            start_time = time.time()
             while time.time() - start_time < time_limit:
-                # read a frame
+                # capture a frame
                 ret, frame = cap.read()
-                # apply background subtraction
-                fgmask = fgbg.apply(frame)
                 # write the frame to the video file
                 out.write(frame)
-                # count non-zero pixels in the mask
-                count = cv2.countNonZero(fgmask)
-                # if motion is no longer detected, stop recording
-                if count <= 100:
-                    print("Motion stopped! Video recorded!")
-                    break
                 # display the frame
                 cv2.imshow('frame', frame)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-            break
-        # display the frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# release the capture and writer
+            # release the writer
+            out.release()
+            cv2.destroyAllWindows()
+            print("Video recorded!")
+        
+        # reset motion detection parameters
+        motion_detected = False
+        motion_frame = None
+    
+    # wait before capturing the next frame
+    time.sleep(capture_interval)
+    
+# release the capture
 cap.release()
-cv2.destroyAllWindows()
-if mode == 'video':
-    out.release()
