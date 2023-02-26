8ab15345bb96bcae7f0a6b38cbf4245aebea20af
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-02-26 20:47:50 +0100

    Commit

--- motcam_script.py
+++ motcam_script.py
@@ -14,38 +14,73 @@ config.read(config_path)
 mode = config.get('config', 'mode')
 time_limit = int(config.get('config', 'time'))
 
+# create capture object
+cap = cv2.VideoCapture(0)
+# create background subtractor
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
 if mode == 'photo':
     print("Capturing photo...")
-    # initialize video capture
-    cap = cv2.VideoCapture(0)
-    image_path = f'motion_on{time.ctime()}.png'
-    # capture a single frame
-    ret, frame = cap.read()
-    # save the captured frame
-    cv2.imwrite('Captures/' + image_path, frame)
-    # release the capture
-    cap.release()
-    print("Photo captured!")
-else:
+    while True:
+        # read a frame
+        ret, frame = cap.read()
+        # apply background subtraction
+        fgmask = fgbg.apply(frame)
+        # count non-zero pixels in the mask
+        count = cv2.countNonZero(fgmask)
+        # if motion is detected, take a photo
+        if count > 100:
+            image_path = f'motion_on{time.ctime()}.png'
+            cv2.imwrite('Captures/' + image_path, frame)
+            print("Photo captured!")
+            break
+        # display the frame
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+elif mode == 'video':
     print("Recording video...")
-    # initialize video capture
-    cap = cv2.VideoCapture(0)
     # initialize video writer
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('motion_video.avi', fourcc, 20.0, (640, 480))
     # start recording
     start_time = time.time()
-    while time.time() - start_time < time_limit:
-        # capture a frame
+    while True:
+        # read a frame
         ret, frame = cap.read()
-        # write the frame to the video file
-        out.write(frame)
+        # apply background subtraction
+        fgmask = fgbg.apply(frame)
+        # count non-zero pixels in the mask
+        count = cv2.countNonZero(fgmask)
+        # if motion is detected, start recording
+        if count > 100:
+            print("Motion detected! Recording video...")
+            while time.time() - start_time < time_limit:
+                # read a frame
+                ret, frame = cap.read()
+                # apply background subtraction
+                fgmask = fgbg.apply(frame)
+                # write the frame to the video file
+                out.write(frame)
+                # count non-zero pixels in the mask
+                count = cv2.countNonZero(fgmask)
+                # if motion is no longer detected, stop recording
+                if count <= 100:
+                    print("Motion stopped! Video recorded!")
+                    break
+                # display the frame
+                cv2.imshow('frame', frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            break
         # display the frame
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    # release the capture and writer
-    cap.release()
+
+# release the capture and writer
+cap.release()
+cv2.destroyAllWindows()
+if mode == 'video':
     out.release()
-    cv2.destroyAllWindows()
-    print("Video recorded!")
