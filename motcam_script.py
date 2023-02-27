dabdef2a08ea44c4af592188ee3ab12a6811f0cf
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-02-27 20:54:47 +0100

    finaLLY??

--- motcam_script.py
+++ motcam_script.py
@@ -1,63 +1,75 @@
 import configparser
 import cv2
 import time
-from pathlib import Path
 import os
 
 # Load configuration settings from config.ini file
-
-path = Path(__file__)
-ROOT_DIR = path.parent.absolute()
-config_path = os.path.join(ROOT_DIR, "config.ini")
-
 config = configparser.ConfigParser()
-config.read(config_path)
+config.read('config.ini')
 mode = config.get('Settings', 'mode')
 time_limit = config.getint('Settings', 'time')
 is_running = config.getboolean('Settings', 'is_running')
 
 # Initialize camera and motion detector
-camera = cv2.VideoCapture(0)
+camera = None
 detector = cv2.createBackgroundSubtractorMOG2()
 
 # Create folder to store captured files if it does not already exist
-import os
 if not os.path.exists('Captures'):
     os.makedirs('Captures')
 
 # Main loop to capture photos or videos when motion is detected
 while is_running:
-    # Capture a frame from the camera and convert it to grayscale
-    ret, frame = camera.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    try:
+        # Initialize camera if it has not already been initialized
+        if camera is None:
+            camera = cv2.VideoCapture(0)
 
-    # Apply background subtraction to the frame to detect motion
-    mask = detector.apply(gray)
+        # Capture a frame from the camera and convert it to grayscale
+        ret, frame = camera.read()
+        if not ret:
+            raise Exception("Error: Camera read failed")
 
-    # Check if enough motion has been detected to trigger a capture
-    motion = (cv2.countNonZero(mask) > 1000)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # If motion has been detected, capture a photo or video based on the configuration settings
-    if motion:
-        filename = time.strftime('%Y%m%d-%H%M%S')
-        if mode == 'photo':
-            cv2.imwrite(f'Captures/{filename}.jpg', frame)
-        elif mode == 'video':
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            out = cv2.VideoWriter(f'Captures/{filename}.mp4', fourcc, 20.0, (640, 480))
-            start_time = time.time()
-            while (time.time() - start_time) < time_limit:
-                ret, frame = camera.read()
-                out.write(frame)
-            out.release()
+        # Apply background subtraction to the frame to detect motion
+        mask = detector.apply(gray)
 
-    # Wait for a short period of time before capturing the next frame
-    time.sleep(0.1)
+        # Check if enough motion has been detected to trigger a capture
+        motion = (cv2.countNonZero(mask) > 1000)
 
-    # Check the is_running setting in the config file to see if the loop should be stopped
-    config.read(config_path)
-    is_running = config.getboolean('Settings', 'is_running')
+        # If motion has been detected, capture a photo or video based on the configuration settings
+        if motion:
+            filename = time.strftime('%Y%m%d-%H%M%S')
+            if mode == 'photo':
+                cv2.imwrite(f'Captures/{filename}.jpg', frame)
+            elif mode == 'video':
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                out = cv2.VideoWriter(f'Captures/{filename}.mp4', fourcc, 20.0, (640, 480))
+                start_time = time.time()
+                while (time.time() - start_time) < time_limit:
+                    ret, frame = camera.read()
+                    if not ret:
+                        raise Exception("Error: Camera read failed")
+                    out.write(frame)
+                out.release()
+
+        # Wait for a short period of time before capturing the next frame
+        time.sleep(0.1)
+
+        # Check the is_running setting in the config file to see if the loop should be stopped
+        config.read('config.ini')
+        is_running = config.getboolean('Settings', 'is_running')
+
+    except KeyboardInterrupt:
+        # If the user presses CTRL+C, release the camera and video writer objects
+        print("\nStopping the script...")
+        if camera is not None:
+            camera.release()
+        cv2.destroyAllWindows()
+        break
 
 # Release the camera and video writer objects
-camera.release()
+if camera is not None:
+    camera.release()
 cv2.destroyAllWindows()
