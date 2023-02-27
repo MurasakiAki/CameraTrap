f844591a17a7321c77c6b3c93df4c23ced0e527d
MurasakiAki <akkishia88@gmail.com>
2023-02-27 13:59:17 +0100

    Commit

--- motcam_script.py
+++ motcam_script.py
@@ -33,41 +33,41 @@ frame_count = 0
 while True:
     # capture a frame
     ret, frame = cap.read()
-
+    
     # increment frame count
     frame_count += 1
 
     # convert frame to grayscale for motion detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
+    
     # set initial motion frame
     if motion_frame is None:
         motion_frame = gray
         continue
-
+    
     # detect motion every nth frame
     if frame_count % motion_detection_interval == 0:
-        # calculate difference between current frame and motion frame
+    # calculate difference between current frame and motion frame
         frame_delta = cv2.absdiff(motion_frame, gray)
         thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
-
+    
         # dilate the thresholded image to fill in holes
         thresh = cv2.dilate(thresh, None, iterations=2)
-
+    
         # find contours in the thresholded image
         contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+    
         # check if motion is detected
         for c in contours:
             if cv2.contourArea(c) < 500:
                 continue
             motion_detected = True
-
+    
         # reset motion frame if no motion is detected
         if not motion_detected:
             motion_frame = gray
-
+    
         # capture photo or record video if motion is detected
         if motion_detected:
             if mode == 'photo':
@@ -84,7 +84,7 @@ while True:
                 while time.time() - start_time < time_limit:
                     # capture a frame
                     ret, frame = cap.read()
-                    # write the frame to the video file
+                    # write the frame to the video file 
                     out.write(frame)
                     # display the frame
                     cv2.imshow('frame', frame)
@@ -94,10 +94,11 @@ while True:
                 out.release()
                 cv2.destroyAllWindows()
                 print("Video recorded!")
-
+        
             # reset motion detection parameters
             motion_detected = False
             motion_frame = None
             frame_count = 0
-
+    
             #
+    
