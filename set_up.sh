d4b12f2a1849f2d7620fbf3f40e9fa94b10fdd8d
MurasakiAki <akkishia88@gmail.com>
2023-03-01 11:02:56 +0100

    Start web

--- set_up.sh
+++ set_up.sh
@@ -6,7 +6,7 @@ chmod u+x motcam
 # Add the current directory to the PATH variable
 export PATH=$PATH:$(pwd)
 
-# Save the updated PATH variable to the bashrc file
+# Save the updated PATH variable to the bashrc or profile file - depends on the system
 echo "export PATH=\$PATH:$(pwd)" >> ~/.profile
 #echo "export PATH=\$PATH:$(pwd)" >> ~/.bashrc
 
