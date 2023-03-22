357eb40f4d42f199e0e1e1a0756561e9e4ba0a2d
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-03-22 20:36:01 +0100

    commit
    making of picture showing on page

--- server.js
+++ server.js
@@ -47,6 +47,34 @@ const server = http.createServer((req, res) => {
         res.end(content);
       }
     });
+  } else if (req.url === '/script.js') {
+    const filePath = path.join(__dirname, 'script.js');
+    const contentType = 'text/javascript';
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end(`Error loading ${filePath}: ${err}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf-8');
+      }
+    });
+  } else if (req.url === '/picture') {
+    const pictures = ['picture1', 'picture2', 'picture3', 'picture4']; // Array of picture names
+    const randomIndex = Math.floor(Math.random() * pictures.length); // Get a random index into the array
+    const filePath = path.join(__dirname, 'pictures', pictures[randomIndex] + '.jpg'); // Build the path to the randomly selected picture
+    const contentType = 'image/jpeg';
+
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        res.writeHead(500);
+        res.end(`Error loading ${filePath}: ${err}`);
+      } else {
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content);
+      }
+    });
   } else {
     // Handle other requests
     res.writeHead(404);
